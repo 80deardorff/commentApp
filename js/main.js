ba5399a6d00eedb20b411264941132d77b8b1437
Adrian Deardorff <80deardorff@live.com>
2018-06-29 12:19:20 -0700

    created all elements in js on button click

--- js/main.js
+++ js/main.js
@@ -6,5 +6,23 @@ function currentTime() {
   let meridies = 'am';
   if (hour > 12) {meridies = 'pm';};
   if (hour > 12) {hour = hour - 12;};
-  return hour + ':' + minute + ' ' + meridies;
+  return hour + ':' + minute + meridies;
+}
+
+// CREATE A VARIABLE THAT STORES THE VALUE OF THE INPUT AFTER THE CLICK OF THE BUTTON
+let input = document.getElementById('input');
+let inner = document.getElementById('inner');
+
+function post() {
+  let p = document.createElement('p');
+  inner.appendChild(p);
+  let img = document.createElement('img');
+  img.setAttribute('class', 'pr-2');
+  img.src = 'image/user.png';
+  img.alt = 'User image.';
+  p.appendChild(img);
+  p.textContent = input.value;
+  let span = document.createElement('span');
+  p.appendChild(span);
+  input.value = '';
 }
