cbe20a5a69091473ce16eb403e6cebdd7e31b5ab
Adrian Deardorff <80deardorff@live.com>
2018-06-30 23:45:51 -0700

    cleaned up js

--- js/main.js
+++ js/main.js
@@ -9,20 +9,26 @@ function currentTime() {
   return hour + ':' + minute + meridies;
 }
 
-// CREATE A VARIABLE THAT STORES THE VALUE OF THE INPUT AFTER THE CLICK OF THE BUTTON
+// CREATE A VARIABLE THAT STORES THE VALUE OF THE INPUT
 let input = document.getElementById('input');
+// CREATE A VARIABLE THAT STORES DIV WITH ID OF INNER
 let inner = document.getElementById('inner');
 
+// CREATE MAIN FUNCTION THAT ADDS A PARAGRAPH TO THE BODY AND INPUTS THE VALUE OF THE MAIN INPUT INTO THE NEWLY CREATED PARAGRAPH
 function post() {
+  // VARIABLE FOR PARAGRAPH
   let p = document.createElement('p');
   inner.appendChild(p);
+  p.textContent = input.value;
+  // VARIABLE FOR IMAGE
   let img = document.createElement('img');
   img.setAttribute('class', 'pr-2');
   img.src = 'image/user.png';
   img.alt = 'User image.';
   p.appendChild(img);
-  p.textContent = input.value;
+  // VARIABLE FOR SPAN
   let span = document.createElement('span');
-  p.appendChild(span);
+  p.appendChild(document.createTextNode(span));
+  // RESET VALUE OF INPUT TO BLANK
   input.value = '';
 }
