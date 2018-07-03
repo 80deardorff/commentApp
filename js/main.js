827656a9cea2d74074a80a662769b68c17f9a404
Adrian Deardorff <80deardorff@live.com>
2018-07-03 16:26:07 -0700

    added code to not allow function to run if nothing is written inside the input

--- js/main.js
+++ js/main.js
@@ -17,49 +17,43 @@ let inner = document.getElementById('inner');
 
 // CREATE MAIN FUNCTION THAT ADDS A PARAGRAPH TO THE BODY AND INPUTS THE VALUE OF THE MAIN INPUT INTO THE NEWLY CREATED PARAGRAPH
 function post() {
-  // VARIABLE FOR IMAGE
-  let div1 = document.createElement('div');
-  div1.setAttribute('class', 'one col-1' );
-  inner.appendChild(div1);
-  let img = document.createElement('img');
-  img.src = 'image/user.png';
-  img.alt = 'User image.';
-  div1.appendChild(img);
-  // VARIABLE FOR A DIV
-  let div2 = document.createElement('div');
-  div2.setAttribute('class', 'two col-11');
-  inner.appendChild(div2);
-  // VARIABLE FOR PARAGRAPH
-  let p = document.createElement('p');
-  div2.appendChild(p);
-  p.textContent = input.value;
-  // VARIABLE FOR SPAN
-  let span = document.createElement('span');
-  div2.appendChild(span);
-  span.textContent = ' ' + currentTime() + ' ';
-  // VARIABLE FOR HEART ICON
-  let i = document.createElement('i');
-  i.setAttribute('class', 'fas fa-heart');
-  div2.appendChild(i);
-  // RESET VALUE OF INPUT TO BLANK
-  input.value = '';
-  // SCROLL TO BOTTOM OF DIV
-  inner.scrollTo(0, inner.scrollHeight);
+  // IF STATEMENT THAT ONLY RUNS THE CODE IF SOMETHING IS IN THE INPUT
+  if (input.value != '') {
+    // VARIABLE FOR IMAGE
+    let div1 = document.createElement('div');
+    div1.setAttribute('class', 'one col-1' );
+    inner.appendChild(div1);
+    let img = document.createElement('img');
+    img.src = 'image/user.png';
+    img.alt = 'User image.';
+    div1.appendChild(img);
+    // VARIABLE FOR A DIV
+    let div2 = document.createElement('div');
+    div2.setAttribute('class', 'two col-11');
+    inner.appendChild(div2);
+    // VARIABLE FOR PARAGRAPH
+    let p = document.createElement('p');
+    div2.appendChild(p);
+    p.textContent = input.value;
+    // VARIABLE FOR SPAN
+    let span = document.createElement('span');
+    div2.appendChild(span);
+    span.textContent = ' ' + currentTime() + ' ';
+    // VARIABLE FOR HEART ICON
+    let i = document.createElement('i');
+    i.setAttribute('class', 'fas fa-heart');
+    div2.appendChild(i);
+    // RESET VALUE OF INPUT TO BLANK
+    input.value = '';
+    // SCROLL TO BOTTOM OF DIV
+    inner.scrollTo(0, inner.scrollHeight);
+  }
 }
 
+// EVENT LISTENER THAT MAKES ENTER KEY CLICK BUTTON
 input.addEventListener("keyup", function(event) {
   event.preventDefault();
   if (event.keyCode === 13) {
     document.getElementById("myBtn").click();
   }
 });
-
-// WHY DOESNT THIS WORK?
-//
-// function accept() {
-//   if (input.value = "") {
-//     console.log('true');
-//   } else {
-//     console.log('false');
-//   }
-// }
